9012ee82aef596c202c3c8df6aa5e0e932e79f49
Aidan Markham <apm4337@rit.edu>
2016-03-05 20:43:21 -0500

    Added Grader and answer generator

---
+++ Mastermind/Mastermind.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastermind
+{
+    class Mastermind
+    {
+        List<int> solution; // list to hold the solution code
+        Random rand; // for randomly generating a solution
+        List<int> testSolution;
+        public Mastermind()
+        {
+            rand = new Random();
+            solution = new List<int>();
+        }
+        public void Initialize()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                //solution[i] = rand.Next(0, 8);
+                solution.Add(i);
+            }
+        }
+
+
+        public int Grade(List<int> guess)
+        {
+            int colorMatch = 0;
+            int fullMatch = 0;
+            testSolution = new List<int>(solution);
+            
+
+            for (int i = 0; i < guess.Count; i++)
+            {
+                if (guess[i] == solution[i])
+                {
+                    fullMatch += 1;
+                }
+            }
+
+            for (int i = 0; i < guess.Count; i++)
+            {
+                Predicate<int> matches = x => x == guess[i];
+                
+                if (testSolution.FindAll(matches).Count() > 0)
+                {
+                    colorMatch += 1;
+                    testSolution[testSolution.IndexOf(guess[i])] = 10;
+                }
+            }
+
+            
+
+            return fullMatch;
+        }
+    }
+}
--- Mastermind/Program.cs
+++ Mastermind/Program.cs
@@ -8,8 +8,26 @@ namespace Mastermind
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            Mastermind mastermind = new Mastermind();
+            mastermind.Initialize();
+
+            Random rand = new Random();
+
+
+
+            int answer = 0;
+            int count = 0;
+
+            while (answer != 5)
+            {
+                count += 1;
+                answer = mastermind.Grade(new List<int>(new int[] { rand.Next(0, 8), rand.Next(0, 8), rand.Next(0, 8), rand.Next(0, 8), rand.Next(0, 8) }));
+            }
+
+            Console.WriteLine("Guesses: " + count);
         }
     }
 }
